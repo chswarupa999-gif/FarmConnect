b53796c9126a72363d28dabda1f2a4a9efa0312b
Swarupa444 <swarupachuduri444@gmail.com>
2026-07-18 23:05:46 +0530

    Production backend URL

--- client/app/services/api.js
+++ client/app/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://farmconnect-backend-g6tx.onrender.com/api",
 });
 
 // Automatically attach JWT token
